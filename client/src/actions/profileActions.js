e3a75259909a1267e1d1e39b2f0e9c074f2b49da
Chocotunda <kdgganpat@gmail.com>
2019-04-27 14:39:09 +0200

    added experience form functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -35,6 +35,15 @@ export const createProfile = (profileData, history) => dispatch => {
     );
 };
 
+// Add experience
+export const addExperience = (expData,
+history) => dispatch => {
+  axios
+    .post('/api/profile/experience', expData)
+    .then(res => history.push('/dashboard'))
+    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
 // Delete account & Profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm('Are you sure? This can not be undone!')) {
