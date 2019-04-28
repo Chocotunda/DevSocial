e91e0107d213bc58c9076760bf265e714a2f085a
Chocotunda <kdgganpat@gmail.com>
2019-04-28 20:38:01 +0200

    server fix

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use(bodyParser.json());
 // DB Config
 const db = require('./config/keys').mongoURI;
 
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
@@ -36,7 +36,7 @@ app.use('/api/posts', posts);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  // Static folder
+  // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
@@ -44,6 +44,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
